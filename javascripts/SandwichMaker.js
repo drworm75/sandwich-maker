2a765762308e9f4dd49699bc94ff7c301a74de68
drworm75 <dwgames@gmail.com>
2017-02-28 18:26:11 -0600

    project meets all requirments

--- javascripts/SandwichMaker.js
+++ javascripts/SandwichMaker.js
@@ -9,14 +9,17 @@ var SandwichMaker = (function() {
   					"veggies" : 0	
   };
 
+  var amount = document.getElementById("amount");
+
   // Return the public interface that other code can interact with
   return {
     addTopping: function(toppingKey, toppingTotal) {
     	totalPrice[toppingKey] = toppingTotal;
     	var finalPrice = totalPrice.breads + totalPrice.cheeses +totalPrice.condiments + totalPrice.meats + totalPrice.veggies;
-    	console.log("Here's a total!", finalPrice.toFixed(2));
+    	
  
       console.log("Total" , totalPrice)
+      amount.innerHTML = finalPrice;
     }
   };
 })();
